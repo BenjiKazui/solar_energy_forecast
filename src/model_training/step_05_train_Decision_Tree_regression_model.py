fa1c91ded40f9845c6179c6f9103d502c490f9a3
BenjiKazui <B.Rudolf2014@gmail.com>
2025-03-16 13:25:08 +0100

    created some more models and trained them, started to create a script to train multiple models added the models to gitignore

--- src/model_training/step_05_train_Decision_Tree_regression_model.py
+++ src/model_training/step_05_train_Decision_Tree_regression_model.py
@@ -1,7 +1,7 @@
 from src.step_03_data_preprocessing import data_preprocessing
 from src.step_04_feature_engineering import create_time_based_features, create_lag_features, create_sun_position_features, create_interaction_features, create_fourier_features
 
-from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error as mae
 import pandas as pd
@@ -17,23 +17,23 @@ pd.set_option("display.max_colwidth", None)  # Don't truncate cell content
 
 X, y = data_preprocessing()
 
-# LOL using additionally time_based_features with the simple LinearRegressionModel gives a MAE of 220, not using them gives a MAE of 174.77
-# no added features: MAE 174.77
-# only added time_based_features: MAE 220
-# only added sun_position_features: MAE 226
-# time + sun_position features: MAE  266
-# time + sun + lag: MAE 241
-# time + sun + lag + interaction: MAE 232
-# lag + interaction: MAE 173
-# only lag: MAE 176
-# only interaction: MAE 176
-# time + lag + interaction: MAE 234
-# lag + sun + interaction: MAE 215
+
+# no added features: MAE 160
+# only added time_based_features: MAE 187
+# only added sun_position_features: MAE 156
+# time + sun_position features: MAE 169
+# time + sun + lag: MAE 169
+# time + sun + lag + interaction: MAE 169
+# lag + interaction: MAE 164
+# only lag: MAE 160
+# only interaction: MAE 164
+# time + lag + interaction: MAE 176
+# lag + sun + interaction: MAE 156 -> 127 (max_depth=5) -> 124 (max_depth=7) -> 131 (max_depth=10)
 
 
 #X = create_time_based_features(X)
 X = create_lag_features(X)
-#X = create_sun_position_features(X)
+X = create_sun_position_features(X)
 X = create_interaction_features(X)
 
 
@@ -63,7 +63,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 #print(X_test)
 #print(y_test)
 
-model_lr = LinearRegression()
+# changing alpha doenst change the MAE a lot
+model_lr = DecisionTreeRegressor(max_depth=10, random_state=42)
 model_lr.fit(X_train, y_train)
 
 y_pred = model_lr.predict(X_test)
@@ -79,4 +80,4 @@ plt.plot(results["y_test"], label="y_test")
 plt.plot(results["y_pred"], label="y_pred")
 plt.show()
 
-#joblib.dump(model_lr, "C:/Users/Brudo/solar_energy_forecast/models/linear_regression_model.pkl")
+#joblib.dump(model_lr, "C:/Users/Brudo/solar_energy_forecast/models/Decision_Tree_regression_model.pkl")
--- src/model_training/step_05_train_Random_Forest_regression_model.py
+++ src/model_training/step_05_train_Random_Forest_regression_model.py
@@ -1,7 +1,7 @@
 from src.step_03_data_preprocessing import data_preprocessing
 from src.step_04_feature_engineering import create_time_based_features, create_lag_features, create_sun_position_features, create_interaction_features, create_fourier_features
 
-from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_absolute_error as mae
 import pandas as pd
@@ -17,23 +17,23 @@ pd.set_option("display.max_colwidth", None)  # Don't truncate cell content
 
 X, y = data_preprocessing()
 
-# LOL using additionally time_based_features with the simple LinearRegressionModel gives a MAE of 220, not using them gives a MAE of 174.77
-# no added features: MAE 174.77
-# only added time_based_features: MAE 220
-# only added sun_position_features: MAE 226
-# time + sun_position features: MAE  266
-# time + sun + lag: MAE 241
-# time + sun + lag + interaction: MAE 232
-# lag + interaction: MAE 173
-# only lag: MAE 176
-# only interaction: MAE 176
-# time + lag + interaction: MAE 234
-# lag + sun + interaction: MAE 215
+
+# no added features: MAE 144
+# only added time_based_features: MAE 165
+# only added sun_position_features: MAE 107
+# time + sun_position features: MAE 116
+# time + sun + lag: MAE 116
+# time + sun + lag + interaction: MAE 115
+# lag + interaction: MAE 134
+# only lag: MAE 135
+# only interaction: MAE 144
+# time + lag + interaction: MAE 149
+# lag + sun + interaction: MAE 106 -> 107 (n_estimators=50) -> 106 (n_estimators=120) -> 106 (n_estimators=200)
 
 
 #X = create_time_based_features(X)
 X = create_lag_features(X)
-#X = create_sun_position_features(X)
+X = create_sun_position_features(X)
 X = create_interaction_features(X)
 
 
@@ -63,7 +63,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 #print(X_test)
 #print(y_test)
 
-model_lr = LinearRegression()
+# changing alpha doenst change the MAE a lot
+model_lr = RandomForestRegressor(n_estimators=100, random_state=42)
 model_lr.fit(X_train, y_train)
 
 y_pred = model_lr.predict(X_test)
@@ -79,4 +80,4 @@ plt.plot(results["y_test"], label="y_test")
 plt.plot(results["y_pred"], label="y_pred")
 plt.show()
 
-#joblib.dump(model_lr, "C:/Users/Brudo/solar_energy_forecast/models/linear_regression_model.pkl")
+joblib.dump(model_lr, "C:/Users/Brudo/solar_energy_forecast/models/Random_Forest_regression_model.pkl")
